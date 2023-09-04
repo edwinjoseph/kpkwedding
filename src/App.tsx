0102f941687c3b7025feb14b5b4e2a49b63c2f97
Edwin Joseph <edwinj@rightmove.co.uk>
2023-09-04 22:27:20 +0100

    fix: update heading line height and main element padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import type { Component } from 'solid-js';
 
 const App: Component = () => (
-    <main className="flex flex-col justify-center min-h-screen">
-        <h1 className="font-heading my-[30px] text-[64px] text-center md:text-[120px]">Kezia & James</h1>
+    <main className="flex flex-col justify-center min-h-screen p-4">
+        <h1 className="font-heading my-[30px] text-[64px] leading-[64px] text-center md:text-[120px] md:leading-[120px]">Kezia & James</h1>
         <h2 className="uppercase text-2xl text-center font-bold">The wedding - Coming soon</h2>
     </main>
 );
