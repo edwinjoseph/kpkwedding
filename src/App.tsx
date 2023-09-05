56fceaf6037302fe94d3fb17cdedcc0fe7efe344
Edwin Joseph <edwinj@rightmove.co.uk>
2023-09-05 21:11:09 +0100

    feat: update letting spacing on header

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ const App = () => (
     <>
         <header className="bg-white fixed w-full z-10 top-0">
             <div className="flex justify-between px-4 py-6 max-w-[1440px] mx-auto md:px-10">
-                <p className="uppercase font-bold">London</p>
-                <p className="uppercase font-bold before:content-['16.08.24'] md:before:content-['16_august_2024']" />
+                <p className="uppercase font-bold tracking-[3px] md:tracking-[4px]">London</p>
+                <p className="uppercase font-bold tracking-[3px] md:tracking-[4px] before:content-['16.08.24'] md:before:content-['16_august_2024']" />
             </div>
         </header>
         <main className="mt-[72px] px-4 md:px-10">
