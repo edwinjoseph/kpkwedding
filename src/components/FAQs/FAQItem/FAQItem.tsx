902483fae2d2e71abea122fd25f842fec9723329
Edwin Joseph <edwinj3829@googlemail.com>
2023-09-15 18:42:24 +0000

    fix: correctly align faq item titles

--- src/components/FAQs/FAQItem/FAQItem.tsx
+++ src/components/FAQs/FAQItem/FAQItem.tsx
@@ -11,7 +11,7 @@ export interface FAQItemProps {
 
 const FAQItem = ({ title, content, isOpen, onClick }: FAQItemProps) => (
     <div class="relative border-t border-[#CBCBCB]">
-        <button class="flex justify-between w-full items-center pt-4 md:pt-6 pb-4 md:pb-6" onClick={onClick}>
+        <button class="flex justify-between w-full justify-items-start items-center pt-4 md:pt-6 pb-4 md:pb-6" onClick={onClick}>
             <h3 class="font-bold md:text-2xl">{title}</h3>
             <div
                 class={cx('relative w-6 h-0.5 top-1/2 bg-[#8D8D8D] before:content-[""] before:block before:absolute before:h-3 before:w-0.5 before:bg-[#8D8D8D] before:-top-[11px] before:left-1/2 before:-translate-x-1/2 before:origin-bottom before:transition-transform after:content-[""] after:block after:absolute after:h-3 after:top-px after:w-0.5 after:bg-[#8D8D8D] after:left-1/2 after:origin-top after:-translate-x-1/2 after:transition-transform', {
