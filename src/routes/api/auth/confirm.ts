96b31e0226610cb3b6163f434f5a0a736141dbd9
Edwin Joseph <edwinj@rightmove.co.uk>
2023-09-18 10:40:13 +0100

    feat: use get host utility instead of request origin

--- src/routes/api/auth/confirm.ts
+++ src/routes/api/auth/confirm.ts
@@ -1,34 +1,34 @@
 import { APIEvent, redirect, json } from 'solid-start';
 import supabase from '@lib/supabase/server';
 import setCookieHeaders from '@utils/set-cookie-headers';
+import getHost from '@utils/get-host';
 
 export async function GET({ request }: APIEvent) {
-    const { origin, searchParams } = new URL(request.url)
+    const { searchParams } = new URL(request.url)
     const token_hash = searchParams.get('token_hash');
 
     if (!token_hash) {
-        return redirect(new URL(`/login?error=0001`, origin).toString());
+        return redirect(new URL(`/login?error=0001`, getHost()).toString());
     }
 
     const { data, error } = await supabase(request).auth.verifyOtp({ token_hash, type: 'email' });
 
     if (error) {
-        return redirect(new URL(`/login?error=0003`, origin).toString());
+        return redirect(new URL(`/login?error=0003`, getHost()).toString());
     }
 
     const cookieHeaders = setCookieHeaders(data.session)
 
     if (!cookieHeaders) {
-        return redirect(new URL(`/login?error=0004`, origin).toString());
+        return redirect(new URL(`/login?error=0004`, getHost()).toString());
     }
 
-    return redirect(new URL(`/admin`, origin).toString(), {
+    return redirect(new URL(`/admin`, getHost()).toString(), {
         headers: cookieHeaders
     });
 }
 
 export async function POST({ request }: APIEvent) {
-    const { origin } = new URL(request.url)
     const body = await new Response(request.body).json();
 
     if (!body.email && !body.code) {
@@ -66,7 +66,7 @@ export async function POST({ request }: APIEvent) {
     return json({ ok: true }, {
         headers: [
             ...cookieHeaders,
-            ['Location', new URL('/admin', origin).toString()]
+            ['Location', new URL('/admin', getHost()).toString()]
         ]
     });
 }
