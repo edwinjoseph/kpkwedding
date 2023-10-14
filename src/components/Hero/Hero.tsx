52dea79a221602caec8cf686fdcab3c539994287
Edwin Joseph <edwinj@rightmove.co.uk>
2023-10-14 04:06:00 +0100

    feat: break hero title on mobile

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => (
     <Section>
         <Section.Container>
             <div class={`mx-auto flex aspect-[343/540] max-w-[1369px] flex-col justify-center bg-[url('/assets/hero-mobile.jpg')] bg-cover bg-top md:aspect-[377/505] md:bg-[url('/assets/hero-tablet.jpg')] lg:aspect-[413/234] lg:bg-[url('/assets/hero.jpg')]`}>
-                <h1 class="relative z-20 my-[30px] text-center font-heading text-[64px] leading-[64px] text-white md:text-[120px] md:leading-[120px]">Kezia & James</h1>
+                <h1 class="relative z-20 my-[30px] text-center font-heading text-[64px] leading-[64px] text-white md:text-[120px] md:leading-[120px]">Kezia <br class="lg:hidden" />& James</h1>
                 <h2 class="text-center text-lg font-bold uppercase tracking-[4px] text-white md:text-2xl">The wedding</h2>
             </div>
             <LearnMoreIcon class="z-20 mx-auto mt-[-30px]" />
