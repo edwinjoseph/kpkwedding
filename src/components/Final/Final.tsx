7e07d3d55cb1d0ee31545ed17079f303cd52d17e
Edwin Joseph <edwinj@rightmove.co.uk>
2023-10-22 11:58:29 +0100

    feat: add scroll to top functionality

--- src/components/Final/Final.tsx
+++ src/components/Final/Final.tsx
@@ -1,17 +1,27 @@
 import Section from '../Section';
 import BackToTopIcon from '../BackToTopIcon';
 
-const Final = () => (
-    <Section class="mb-20 md:mb-20">
-        <Section.Container>
-            <picture>
-                <source media="(max-width: 767px)" srcset="/assets/final-mobile.jpg" />
-                <source media="(min-width: 768px)" srcset="/assets/final.jpg" />
-                <img src="/assets/final.png" alt="Kezia and James" loading="lazy" />
-            </picture>
-            <BackToTopIcon class="mx-auto mt-20" />
-        </Section.Container>
-    </Section>
-)
+const Final = () => {
+    const handleBackToTopClick = () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+
+    return (
+        <Section class="mb-20 md:mb-20">
+            <Section.Container>
+                <picture>
+                    <source media="(max-width: 767px)" srcset="/assets/final-mobile.jpg" />
+                    <source media="(min-width: 768px)" srcset="/assets/final.jpg" />
+                    <img src="/assets/final.png" alt="Kezia and James" loading="lazy" />
+                </picture>
+                <BackToTopIcon class="mx-auto mt-20 cursor-pointer" onClick={handleBackToTopClick} />
+            </Section.Container>
+        </Section>
+    )
+}
 
 export default Final
