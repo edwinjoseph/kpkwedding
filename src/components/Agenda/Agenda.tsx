a8f39a190a479522637bb3933ede482ddc7c2ede
Edwin Joseph <edwinj3829@googlemail.com>
2023-12-09 08:36:52 +0000

    fix: re-add agenda item

--- src/components/Agenda/Agenda.tsx
+++ src/components/Agenda/Agenda.tsx
@@ -7,10 +7,11 @@ const Agenda = () => (
             <Section.Title text="Order of the Day" subtitle="Friday, 16 August 2024" centered />
             <div class="flex flex-col gap-[40px] md:gap-[64px]">
                 <AgendaItem subtitle="Midday" title="Arrive at Asylum Chapel" />
-                <AgendaItem subtitle="12.30pm" title="“We do”" />
+                <AgendaItem subtitle="12:30pm" title="“We do”" />
                 <AgendaItem subtitle="1pm" title="Pop the prosecco" />
-                <AgendaItem subtitle="2.30pm" title="All aboard the wedding bus" />
+                <AgendaItem subtitle="2:30pm" title="All aboard the wedding bus" />
                 <AgendaItem subtitle="3pm" title="Cocktails &amp; canapes at The Lordship Pub" />
+                <AgendaItem subtitle="4:30pm" title="We feast" />
                 <AgendaItem subtitle="6:30pm" title="Evening celebrations" />
                 <AgendaItem subtitle="Midnight" title="We (regrettably) leave" />
             </div>
